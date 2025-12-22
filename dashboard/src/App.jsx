6ab173f66b9bb00feab19a531d09dad22af9e273
rasalaanilyadav <rasalasehitha@gmail.com>
2025-12-22 20:08:44 +0530

    apps changes

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import LandingPage from './vendorDashboard/pages/LandingPage'
-import './app.css'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
-
-
-import"./App.css"
+import "./App.css"
 import NavBar from './vendorDashboard/components/NavBar'
 import Login from './vendorDashboard/components/forms/Login'
-import Notfound from './vendorDashboard/components/Notfound'
+import NotFound from './vendorDashboard/components/NotFound'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-        <Route path='/*' element={<Notfound/>} />
+          <Route path='/' element = {<LandingPage />}/>
+          <Route path='/*' element = {<NotFound />} />
       </Routes>
     </div>
   )
