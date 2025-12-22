979eaefe37fc439cce3f35969fbd80c7c9b09e6b
rasalaanilyadav <rasalasehitha@gmail.com>
2025-12-22 19:27:12 +0530

    comment

--- dashboard/src/vendorDashboard/data/apiPath.js
+++ dashboard/src/vendorDashboard/data/apiPath.js
@@ -1,4 +1,4 @@
-export const API_URL="http://localhost:4000"
+// export const API_URL="http://localhost:4000"
 
 
-// export const API_URL ="https://backend-nodejs-suby-x27e.onrender.com"
+export const API_URL ="https://backend-nodejs-suby-x27e.onrender.com"
